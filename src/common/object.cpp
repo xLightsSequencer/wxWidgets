ee9bf6192d7d6f9aa1821526be11e47ef55b7803
Daniel Kulp <dkulp@apache.org>
2018-10-26 08:13:46 -0400

    Use atomic_int for ref counting

--- src/common/object.cpp
+++ src/common/object.cpp
@@ -338,7 +338,7 @@ void wxRefCounter::DecRef()
 {
     wxASSERT_MSG( m_count > 0, "invalid ref data count" );
 
-    if ( --m_count == 0 )
+    if ( m_count.fetch_sub(1) == 1 )
         delete this;
 }
 
