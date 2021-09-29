a8471cb6bc2d31269bab454975a0ba4e06fb7f45
Daniel Kulp <dkulp@apache.org>
2021-09-29 16:52:11 -0400

    Allow drawing color emoji on windows

--- src/msw/graphicsd2d.cpp
+++ src/msw/graphicsd2d.cpp
@@ -4759,7 +4759,8 @@ void wxD2DContext::DoDrawText(const wxString& str, wxDouble x, wxDouble y)
     GetRenderTarget()->DrawTextLayout(
         D2D1::Point2F(x, y),
         textLayout,
-        fontData->GetBrushData().GetBrush());
+        fontData->GetBrushData().GetBrush(),
+        D2D1_DRAW_TEXT_OPTIONS::D2D1_DRAW_TEXT_OPTIONS_ENABLE_COLOR_FONT);
 }
 
 void wxD2DContext::EnsureInitialized()
