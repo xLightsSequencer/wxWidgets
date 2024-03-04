85d72fec6be1798083c9e4d0062094c0f14f8f34
Daniel Kulp <dan@kulp.com>
2024-03-04 12:46:56 -0500

    Revert "Fix transitory visual glitch with wxBORDER_NONE on GTK3"
    This reverts commit c3d1618cc8e3011646d7fd1de96e89f1606e320a.

--- src/gtk/control.cpp
+++ src/gtk/control.cpp
@@ -92,6 +92,11 @@ void wxControl::PostCreation(const wxSize& size)
 {
     wxWindow::PostCreation();
 
+#ifdef __WXGTK3__
+    if (HasFlag(wxNO_BORDER))
+        GTKApplyCssStyle("*{ border:none; border-radius:0; padding:0 }");
+#endif
+
 #ifndef __WXGTK3__
     // NB: GetBestSize needs to know the style, otherwise it will assume
     //     default font and if the user uses a different font, determined
@@ -102,13 +107,6 @@ void wxControl::PostCreation(const wxSize& size)
 #endif
 
     SetInitialSize(size);
-}
-
-void wxControl::GTKRemoveBorder()
-{
-#ifdef __WXGTK3__
-    GTKApplyCssStyle("*{ border:none; border-radius:0; padding:0 }");
-#endif
 }
 
 // ----------------------------------------------------------------------------
