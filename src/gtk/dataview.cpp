21b89cb328393aed9457941e6b2e95be09358ceb
Daniel Kulp <dan@kulp.com>
2025-03-06 21:22:36 -0500

    Fixes for treeview on linux

--- src/gtk/dataview.cpp
+++ src/gtk/dataview.cpp
@@ -4393,6 +4393,10 @@ wxGtkTreeModelNode *wxDataViewCtrlInternal::FindNode( GtkTreeIter *iter )
 
 wxGtkTreeModelNode *wxDataViewCtrlInternal::FindParentNode( const wxDataViewItem &item )
 {
+    if (m_wx_model == nullptr || !item.IsOk()) {
+        return nullptr;
+    }
+
     wxCHECK_MSG( m_wx_model, nullptr, "no associated model?" );
 
     return FindNode( m_wx_model->GetParent(item) );
